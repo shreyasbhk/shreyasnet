ae026d54ca3a65265a5409431be975f3b8d769d5
Shreyas Hukkeri <hshreyas12@gmail.com>
2016-12-30 23:18:23 -0800

    Added Description and License

--- train.py
+++ train.py
@@ -1,3 +1,35 @@
+#ShreyasNET v1.0
+
+#Copyright (c) 2016 Shreyas Hukkeri
+#
+#Permission is hereby granted, free of charge, to any person obtaining 
+#a copy of this software and associated documentation files (the "Software"), 
+#to deal in the Software without restriction, including without limitation 
+#the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+#and/or sell copies of the Software, and to permit persons to whom the Software 
+#is furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all 
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+#IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+#DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+#ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+#OTHER DEALINGS IN THE SOFTWARE.
+
+
+'''
+The following is the code for building ShreyasNET, a Convolutional Neural 
+Network (CNN) based on the inception model and built using the TFLearn Library. 
+
+The code takes input in the form of 200x200 matrices (the size of which can be 
+    changed by changing the 'matrix_size') from a .h5 file. ShreyasNET is then 
+    trained on the data and the model is saved as a .tflearn file.
+'''
+
 import sys
 from os import remove
 from os.path import isfile
