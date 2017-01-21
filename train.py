64bdd44a74d92f721c588735c6211af57d6e09bb
Shreyas Hukkeri <hshreyas12@gmail.com>
2017-01-21 13:46:25 -0800

    Version 3.0.1
    Version 3 implements multi-threaded and multi-core processing and
    chucked storage during database creation to minimize the stress applied
    on the RAM of the system, while providing optimal speed. Additionally,
    in Version 3, the neural network also takes into account whether the
    cancer, if present, is either malignant or benign. Changes from V3.0.0
    to V3.0.1 include time measurement for measuring performance and
    inclusion of print output functions to make it easier to keep track of
    progress during training.

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-#ShreyasNET v1.10.0
+#ShreyasNET v3.0.1
 
 #Copyright (c) 2016 Shreyas Hukkeri
 #
@@ -44,35 +44,35 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    matrix_size = 2000
-    h5f = h5py.File('data.h5', 'r')
-    X = h5f['X_train']
-    Y = h5f['Y_train']
-
     start_time = time.time()
+    matrix_size = 200
+    h5f = h5py.File('data.h5', 'r')
+    X = h5f['X']
+    Y = h5f['Y']
+
     
 
     conv_input = input_data(shape=[None, matrix_size,matrix_size,1], name='input')
     
-    conv = conv_2d(conv_input, 1, 50, activation='relu', strides=5)
-    conv1 = conv_2d(conv_input, 1, 1, activation='relu', strides=1)
-    conv = flatten(conv)
-    conv1 = flatten(conv1)
+    conv = conv_2d(conv_input, 1, 50, activation='leaky_relu', strides=5)
+    conv1 = conv_2d(conv_input, 1, 1, activation='leaky_relu', strides=1)
+    print(conv)
+    print(conv1)
     
     convnet = merge([conv, conv1], mode='concat', axis=1)
     convnet = dropout(convnet, 0.35)
 
-    convnet = fully_connected(convnet, 10, activation='softmax')
-    convnet = fully_connected(convnet, 2, activation='softmax')
+    convnet = fully_connected(convnet, 10, activation='leaky_relu')
+    convnet = fully_connected(convnet, 2, activation='leaky_relu')
     convnet = regression(convnet, optimizer='adam', learning_rate=0.006, loss='categorical_crossentropy')
     
     model = tflearn.DNN(convnet, tensorboard_verbose=3, tensorboard_dir='Tensordboard/')
     model.fit(X, Y, n_epoch=2, validation_set=0.2, show_metric=True, batch_size=20, snapshot_step=4, 
-        snapshot_epoch=False, run_id='shreyasnet_v1.10.0run-1')
-    model.save('Models/model_v1.10.0_run-1.tflearn')
+        snapshot_epoch=False, run_id='shreyasnet_v3.0.1_run-1')
+    model.save('Models/model_v3.0.1_run-1.tflearn')
     
     end_time = time.time()
-    print("Time:")
+    print("Training Time:")
     print(end_time - start_time)
 
 
